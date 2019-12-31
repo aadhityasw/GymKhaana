7ceefb7344bad2c0d4491033f92820c7c2790ccb
aadhityasw <>
2020-01-01 01:12:59 +0530

    modified login frontend

--- GymKhanna/gymnasium/urls.py
+++ GymKhanna/gymnasium/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.HomePage, name='home-page'),
